a46bf831d6d55470def9ac5d980cb252a7c2bb7a
dxlsunshine <carton@vip.qq.com>
2018-05-23 16:55:39 +0800

    all

--- pillow.py
+++ pillow.py
@@ -1,2 +1,6 @@
-from PIL import Image
-# im = Image.open('test.jpg')
+from PIL import Image,ImageFilter
+im = Image.open('test.jpg')
+w,h = im.size
+print(w,h)
+im2 = im.filter(ImageFilter.BLUR)
+im2.save('blur.jpg','jpeg')
